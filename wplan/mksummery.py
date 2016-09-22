840eda2e8bae1e55aeead9c3ebb7ad2e339ed3a3
labinxu <flowinair@gmail.com>
2016-09-22 08:35:51 -0700

    fixcountnumberfornullcell

--- wplan/mksummery.py
+++ wplan/mksummery.py
@@ -1,12 +1,4 @@
 # -*- coding:utf-8 -*-
-import requests
-import mechanize
-import sys
-if sys.version_info[0]==3:
-    from bs4 import BeautifulSoup
-else:
-    import BeautifulSoup
-
 import utils
 
 # #######################
@@ -15,94 +7,6 @@ INPUT_ARGS = ''
 def debugWritefile(data,filename):
     with open(filename,'w') as f:
         f.write(data)
-
-# jira web
-def login(user, passwd):
-    br = mechanize.Browser()
-    br.set_handle_equiv(True)
-    br.set_handle_redirect(True)  
-    br.set_handle_referer(True)  
-    br.set_handle_robots(False)  
-    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)  
-    br.set_debug_http(False)
-    response = br.open('https://jira01.devtools.intel.com/login.jsp?os_destination=%2Fsecure%2FDashboard.jspa')
-    br.select_form(nr=1)
-    br['os_username'] = INPUT_ARGS.user
-    br['os_password'] = INPUT_ARGS.passwd
-    br.submit()
-    return br
-
-# #######################
-def getComments(jiraId):
-
-    global INPUT_ARGS
-    br = login(INPUT_ARGS.user, INPUT_ARGS.passwd)
-    response = br.open('https://jira01.devtools.intel.com/browse/%s' % jiraId)
-    data = br.response().read()
-    print("Query %s" % jiraId)
-    return parseHtml(data)
-
-
-def parseHtml(data):
-    attrs = {'class':"issue-data-block activity-comment twixi-block  expanded"}
-    soup = BeautifulSoup(data,'html.parser')
-    commentsTag = soup.findAll('div',attrs=attrs)
-    commentAttrs = {'class':'action-body flooded'}
-    comments = {}
-    for itemTag in commentsTag:
-        user = itemTag.find('a', attrs={'class':"user-hover user-avatar"})
-        user = user.text
-
-        time = itemTag.find('time')
-        time = time.text
-
-        content = itemTag.find('div',attrs={'class':"action-body flooded"})
-        content = content.text
-        if not comments.has_key(user):
-            comments[time]=[]
-
-        comments[time].append((user,content))
-
-    return comments
-
-
-def countRowFailuers(data):
-    data = [item.strip().lower() for item in data]
-    count = 0
-    for d in data:
-        if d == 'fail':
-            count += 1
-    return count
-
-
-def countFailuers(data):
-    pass
-
-
-GComments ={}
-def parseComments(jiraId):
-    global GComments
-    content = ''
-    if GComments.has_key(jiraId):
-        content = GComments[jiraId]
-    else:
-        comments = getComments(jiraId)
-        content = doParseComment(comments)
-        GComments[jiraId] = content
-    return content
-
-
-def doParseComment(comments):
-    item = comments.items()[-1]
-    comment = item[-1][-1][-1]
-    return comment
-
-
-def fixStatus(r):
-    if r[9].strip().lower() in ['pending', 'triage', 'verify']:
-        r[9] = 'In Progress'
-    return r
-
 
 def parse(filename):
     result = []
@@ -117,19 +21,11 @@ def parse(filename):
             if(not begin):
                 if(r[0].strip()=='Module'):
                    begin = True
-                result.append(r);
                 continue
             else:
-                if(r[1].strip() == ''):
-                    result.append(r)
-                    continue
-                else:
-                    r = r[0:11]
-                    if len(r)<11:
-                        pass
-                    r = fixStatus(r)
-                    r[-1] = r[-1].strip()
-
+                # status:
+                r = r[0:11]
+                if r[]
             result.append(r)
             failCells = r[2:6]
 
@@ -204,15 +100,6 @@ def cmdline(args=None):
     if(args != None):
         return parser.parse_args(args)
 
-    parser.add_argument('-u','--user',
-                        action='store',
-                        dest='user',
-                        help='user to login')
-
-    parser.add_argument('-p', '--passwd',
-                        action='store',
-                        help='password for user')
-
     parser.add_argument('-s', '--seperator', action="store",
                         dest='seperator',
                         default='|',
@@ -223,21 +110,11 @@ def cmdline(args=None):
                         default='cts_result_st.txt',
                         help='output file')
 
-    parser.add_argument('-t', '--type', action="store",
-                        dest='output_type',
-                        default='txt',
-                        help='output file')
-    
     parser.add_argument('-i', '--input', action='store',
                         dest='input_file',
                         required=True,
                         default='./data/cts_report.txt',
                         help='input file')
-
-    parser.add_argument('-m','--mode',
-                        dest='run_mode',
-                        default='real',
-                        help='run mode debug or real')
 
     INPUT_ARGS = parser.parse_args()
 
