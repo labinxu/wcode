6b7e0d11cb6ef601e345867f2799d66ef3dd4948
labinxu <flowinair@gmail.com>
2016-09-22 09:16:24 -0700

    forsummery

--- wplan/mksummery.py
+++ wplan/mksummery.py
@@ -9,7 +9,12 @@ def debugWritefile(data,filename):
         f.write(data)
 
 def parse(filename):
-    result = []
+
+    result = {}
+    result['In progress']={}
+    result['Patch Ready']={}
+    result['Patch Merged']={}
+    inProgress='In Progress'
     with open(filename) as f:
         begin = False
         counter = False;
@@ -25,71 +30,36 @@ def parse(filename):
             else:
                 # status:
                 r = r[0:11]
-                if r[]
-            result.append(r)
-            failCells = r[2:6]
+                # jiraid, failed number, comments
+                try:
+                    status = r[9].strip()
+                except IndexError,e:
+                    print(l)
+                    continue
+                if not result.has_key(status):
+                    result[status]={}
+                    componment = r[8].strip()
+                    if not result[status].has_key(componment):
+                        result[status][componment]=[]
+                try:
+                    result[status][componment].append("%s %s. %s"%(r[7],r[6],r[-1].strip()))
+                except KeyError,e:
+                    print(status, componment)
 
-            # get comments from jira
-            try:
-                jiraId = utils.GetJiraId(r[7])
-                checkComments = True
-            except Exception,e:
-                jiraId = r[7].strip()
-                # failNum = countRowFailuers(failCells)
-                if jiraId != '':
-                    r[6] = str(-1)#failNum
-                    checkComments = False
 
-            if(jiraId != '' and checkComments):
-                if INPUT_ARGS.run_mode=='real':
-                    comment = parseComments(jiraId)
-                    print("JiraId %s comment %s" % (jiraId, comment))
-
-            # fail number
-            failNumber = len(r)>6 and r[6].strip() or ''
-
-            if(failNumber != ''):
-                if(counterNum != 0):
-                    # set markLine
-                    if counterNum != int(markLine[6].strip()):
-                        markLine[-1] = "%s\%s" % (str(counterNum), markLine[6])
-                        markLine[6] = str(counterNum)
-                    counterNum = 0
-
-                failNumber = int(failNumber)
-                counter = False
-                totalFails=countRowFailuers(failCells)
-                if(failNumber != totalFails):
-                    markLine = r
-                    counterNum = totalFails
-                    counter = True
-            else:
-                if(counter):
-                    # next count
-                    counterNum += countRowFailuers(failCells)
-
-    return result
-
+        return result
 
 def output(result):
-    table = []
-    if INPUT_ARGS.output_type == 'txt':
-        seperator = INPUT_ARGS.seperator
-    else:
-        seperator = ','
-
-    rowFormat = "%s"+seperator
-    for row in result:
-        rowstr = ''
-        for cell in row:
-            if(cell.strip()==''):
-                rowstr += seperator
-            else:
-                rowstr += rowFormat % cell
-        table.append(rowstr+'\n')
+    docxStr=''
+    for status, componments in result.items():
+        docxStr += "*%s\n"%status
+        for  componment, contents in componments.items():
+            docxStr += "**%s\n"%componment
+            for content in contents:
+                docxStr += "***%s\n" % content
 
     with open(INPUT_ARGS.output_file,'w') as f:
-        f.writelines(table)
+        f.writelines(docxStr)
 
 
 def cmdline(args=None):
