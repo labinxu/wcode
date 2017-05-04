c7eb18eec90aa1691e80a59cb2952dc0f08ad6bd
Labin.xu <flowinair@gmail.com>
2017-05-04 18:35:17 +0800

    Update test.py

--- preint/test/test.py
+++ preint/test/test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
 """
 @author LBX
@@ -11,8 +10,7 @@ sys.path.append('..')
 from utils.parser import WebParser
 import utils.utils
 from utils.utils import Browser
-import bl_release_parser
-f = open('../data/loginpage','r')
+#import bl_release_parser
 import unittest
 import dailybuild
 import mechanize
@@ -20,58 +18,30 @@ import urllib2
 import urllib2
 import base64
 import sys
-        
-def LoginBrowser(_user, _mima):
-	print 'Login <--'
-	br = mechanize.Browser()
-	br.set_handle_equiv(True)   
-	br.set_handle_redirect(True)  
-	br.set_handle_referer(True)  
-	br.set_handle_robots(False)  
-	  
-	br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)  
-	br.set_debug_http(False)
-	response = br.open('https://sp2010.ger.ith.intel.com/sites/XMM7360/default.aspx')
-	br.select_form(nr=1)
-	br['os_username'] = _user
-	br['os_password'] = _mima
-	#br['os_username'] = 'huaqianx'
-	#br['os_password'] = 'chq@intel_9'
-	br.submit()
-    
+
 class StubDailyBuilder(dailybuild.DailyBuilder):
     def __init__(self):
+        dailybuild.DailyBuilder.__init__(self)
         pass
     
     def login(self, username, password):
-        print('login======')
-        url = 'https://sp2010.ger.ith.intel.com/sites/XMM7360/default.aspx'
-        request = urllib2.Request(url)
-        base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-        request.add_header("Authorization", "Basic %s" % base64string)   
-        result = urllib2.urlopen(request)
+        preinturl='https://sp2010.ger.ith.intel.com/sites/XMM7360/Lists/7360_Preint/Default.aspx'
+        import requests
+        from requests_kerberos import HTTPKerberosAuth, REQUIRED
+        kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED, sanitize_mutual_error_response=False)
+        r = requests.get(preinturl, auth=kerberos_auth,verify=False)
+        print(r.text.encode('utf-8'))
 
-        #urllib2.HTTPPasswordMgrWithDefaultRealm()
-        #p = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        #
-        #p.add_password(None, loginurl, username, password)
-        #
-        #handler = urllib2.HTTPBasicAuthHandler(p)
-        #opener = urllib2.build_opener(handler)
-        #urllib2.install_opener(opener)
-        #
-        #page = urllib2.urlopen(loginurl).read()
-        #print(page)
-        #
-        #self.browser = Browser()
-        #self.browser.open_without_parser(loginurl)
-        #print(self.browser)
-        #tag = self.browser.find('input', {'name':'csrfmiddlewaretoken'})
-        #post_data = {'csrfmiddlewaretoken': tag.attrs['value'], 'next': ' ',
-        #            'username': username, 'password': password }
-        #
-        #post_data = urllib.urlencode(post_data)
-        #self.browser.submit(loginurl, post_data)
+    def getCurrentTag(self, base):
+        import re
+        bl_releases = 'https://sp2010.ger.ith.intel.com/sites/XMM7360/Lists/BL_Releases/Default.aspx'
+        self.browser.open_with_requests(bl_releases)
+        '''ICE7360_05.1719.05'''
+        #first find ICE7360_05.1719 build
+        pa = re.compile(base+'\.[0-9]{2}')
+        m = pa.search(self.browser.content)
+        return m.group()
+       # print(self.browser.content)
 
 class TestDailyBuilder(unittest.TestCase):
     def test_login_sp2010(self):
@@ -80,7 +50,7 @@ class TestDailyBuilder(unittest.TestCase):
 class TestWebBrowser(unittest.TestCase):
 
     def test_webbrowser(self):
-        browser = Browser(debug=True, content=f.read())
+        browser = Browser(debug=True, content=open('../data/loginpage','r').read())
         tag = browser.find('input',{'name':'csrfmiddlewaretoken'})
         self.assertEqual(tag.get('value'), 'BkkGrr60AQxMcKsBtgL4VQHVtT4WeueS')
 #
@@ -106,8 +76,8 @@ class TestWebBrowser(unittest.TestCase):
        tag1,tag2 = utils.sortTags('ICE7360_05.1719.05', 'ICE7360_05.1719.01')
        self.assertEqual((tag1,tag2),('ICE7360_05.1719.01', 'ICE7360_05.1719.05'))
     def test_bl_release(self):
-        self.assertEqual('ICE7360_05.1719.05', bl_release_parser.bl_release_parser())
-        
+        #self.assertEqual('ICE7360_05.1719.05', bl_release_parser.bl_release_parser())
+        pass
 def load_jason(json_content):
     json_to_python = json.loads(json_content)
     for modem_item in json_to_python['diff']['modem/modem']:
@@ -116,10 +86,12 @@ def load_jason(json_content):
          print(modem_item['UTP'])
 
 def test_login_sp2010():
-    sdb = StubDailyBuilder()
-    sdb.login('labinxux', 'Mar@0303')
-    #LoginBrowser('labinxux', 'Mar@0303')
-    #cur_tag = dbu.getCurrentTag()
+    sdbu = StubDailyBuilder()
+    curBuildName = sdbu.getCurrentTag('ICE7360_05.1719')
+    print('Curbuildname: %s:' % curBuildName)
+    content = sdbu.getReleaseContentXML()
+   # https://utpreloaded.rds.intel.com/CqUtpSms/?Query=149956
+    #open('C:\Users\labinxux\Downloads\ReleaseContent.xml', 'w').write(content)
 def main():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestWebBrowser)
     test_result = unittest.TextTestRunner(verbosity=2).run(suite)
@@ -134,9 +106,26 @@ def main():
    #    print reason
 
 if __name__ == '__main__':
-   # bl_release_parser.bl_release_parser2()
+    from requests_kerberos import HTTPKerberosAuth, REQUIRED
+    import json
+    import requests
+    url = 'https://utpreloaded.rds.intel.com/CqUtpSms/RecentHandling.asmx/AddQuery'
+    payload = {"QueryName":"149956"}
+    headers = {
+    'Accept':'*/*',
+    'Accept-Encoding':'gzip, deflate, br',
+    'Accept-Language':'en-US,en;q=0.8',
+    'Connection':'keep-alive',
+    'Content-Length':'22',
+    'Content-Type':'application/json; charset=UTF-8'}
+    kerberos_auth = HTTPKerberosAuth(mutual_authentication=REQUIRED,
+        sanitize_mutual_error_response=False)
+    r = requests.post(url,auth=kerberos_auth, data=json.dumps(payload), headers=headers, verify=False)
+   # test_login_sp2010()
+    print(r.headers)
+    print(r.text)
    # main()
-   test_login_sp2010()
+    
    
 #    br = Browser()
 #    #json_content = br.open('https://oc6web.intel.com/mani/ICE7360_05.1718.06/ICE7360_05.1718.07/json')
