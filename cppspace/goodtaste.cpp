7ee1097f2b78e161cf4da5254fc96f9744dc7fe7
Labin.xu <flowinair@gmail.com>
2018-11-05 16:59:57 +0800

    Update goodtaste.cpp

--- cppspace/goodtaste.cpp
+++ cppspace/goodtaste.cpp
@@ -1,3 +1,4 @@
+https://linux.cn/article-8498-1.html
 //
 poor taste
 remove_list_entry(entry)
@@ -30,4 +31,21 @@ remove_list_entr(entry){
   } 
   // and just remove it
   *indirect = entry->next;
+}
+////////////////////////////////
+for (r = 0; r < GRID_SIZE; ++r) {
+    for (c = 0; c < GRID_SIZE; ++c) {
+        // Top Edge
+        if (r == 0)
+            grid[r][c] = 0;
+        // Left Edge
+        if (c == 0)
+            grid[r][c] = 0;
+        // Right Edge
+        if (c == GRID_SIZE - 1)
+            grid[r][c] = 0;
+        // Bottom Edge
+        if (r == GRID_SIZE - 1)
+            grid[r][c] = 0;
+    }
 }
