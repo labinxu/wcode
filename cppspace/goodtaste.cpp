0751d43cc628801c8d5c64735778981c78c042eb
Labin.xu <flowinair@gmail.com>
2018-11-05 16:52:22 +0800

    Create goodtaste.cpp

---
+++ cppspace/goodtaste.cpp
@@ -0,0 +1,20 @@
+//
+poor taste
+remove_list_entry(entry)
+{
+prev = NULL;
+walk = head;
+// Walk the list
+while(walk != entry){
+prev = walk;
+walk = walk->next;
+}
+// Remove the entry by updating the head or the previous entry
+if(!prev){
+head = entry->next;
+}
+else{
+prev->next = entr->next;
+}
+
+}
